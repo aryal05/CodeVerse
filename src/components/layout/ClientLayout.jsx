d0fe56df45c119fa0901360a947a9808fb0e082c
aryal05 <aryal.rajat05@gmail.com>
2026-09-14 08:29:21 +0545

    Replace parallax DOM mutations with CSS timelines

--- src/components/layout/ClientLayout.jsx
+++ src/components/layout/ClientLayout.jsx
@@ -5,7 +5,6 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import WhatsAppFloat from '@/components/ui/WhatsAppFloat';
 import CodeVerseLoader from '@/components/ui/CodeVerseLoader';
-import ScrollExperience from '@/components/ui/ScrollExperience';
 
 export default function ClientLayout({ children }) {
   const pathname = usePathname();
@@ -18,7 +17,6 @@ export default function ClientLayout({ children }) {
   return (
     <>
       <CodeVerseLoader enabled />
-      <ScrollExperience />
       <Navbar />
       <main className="public-site-main min-h-screen">
         {children}
